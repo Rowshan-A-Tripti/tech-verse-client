01b62148f8ac40a8467a05611dcf52c1110bf446
tripti <138525545+jhunkar@users.noreply.github.com>
2023-12-04 21:52:40 +0600

    now i create banner section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,10 +1,37 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+
+
 
 
 const Banner = () => {
-    
     return (
         <div>
-           <img className="w-full h-auto md:h-[275px]" src="https://i.ibb.co/fXJpFs0/18774635-6045120gg.jpg" alt="" />
+            <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src="https://i.ibb.co/DgZMWDc/20231203-181724.jpg" /></SwiperSlide>
+        <SwiperSlide><img src="https://i.ibb.co/bFFNVY0/20231203-190151.jpg" /></SwiperSlide>
+        
+      
+      </Swiper>
         </div>
     );
 };
