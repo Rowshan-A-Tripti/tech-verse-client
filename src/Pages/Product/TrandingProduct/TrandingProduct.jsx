51c42c8699e60754e31929e1176c7e67dce5766e
tripti <138525545+jhunkar@users.noreply.github.com>
2023-12-09 15:25:56 +0600

    first commit

--- src/Pages/Product/TrandingProduct/TrandingProduct.jsx
+++ src/Pages/Product/TrandingProduct/TrandingProduct.jsx
@@ -1,11 +1,14 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import { GiVote } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import useAxiosPublic from "../../../Hook/useAxiosPublic";
+import Swal from "sweetalert2";
+import { AuthContext } from "../../../AuthProvider/AuthProvider";
 
 
 const TrandingProduct = () => {
+    const {user} = useContext(AuthContext)
     const [featured, setFeatured] = useState([]);
     const myAxios = useAxiosPublic()
     useEffect(() => {
@@ -14,6 +17,34 @@ const TrandingProduct = () => {
                 setFeatured(res.data)
             })
     }, [myAxios])
+
+    const handlePollUp = (item) => {
+        
+        const upVote = {email: user?.email, voterName: user?.displayName}
+        myAxios.post('/upvotes', upVote)
+        .then(res => {
+            // console.log(res.data);
+            if(res?.data?.insertedId){
+                const findItem = featured.find(items => items?._id == item?._id)
+                const upVote = findItem?.upVote;
+                myAxios.put(`/tranding/${item?._id}`, {upVote: upVote})
+                .then(res => {
+                    console.log(res.data);
+                   if(res?.data?.modifiedCount > 0 ){
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "thanks for vote",
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                      window.location.reload()
+                   }
+                })
+            }
+        })
+       
+    }
 
     
 
@@ -27,21 +58,26 @@ const TrandingProduct = () => {
                     >
                         
 
-                        <div className="card w-96 bg-base-300  h-[550px] shadow-xl rounded-lg">
+                        <div className=" shadow-lg rounded-lg">
+                            <div className="px-4">
+                                <img className="w-full h-48  rounded-lg" src={item?.productImage} alt="" />
+                            </div>
+                            <div className="space-y-5 py-4">
 
-                            <figure className="bg-base-100">
-                                <img className=" mt-8 w-full h-44" src={item?.productImage} />
-                                </figure>
-                            <div className="card-body bg-base-100  rounded-b">
-                                <h2 className="card-title">
-                                {item?.productName}
+                                <div className=" px-4">
+                                    <Link to={'/product'}> <h2 className="text-xl font-semibold text-blue-500">{item.productName}</h2></Link>
+                                </div>
+                                <div className="px-4 flex justify-between items-center">
+
+                                    <p>{item?.tags}</p>
                                     
-                                </h2>
-                                
-                                <p>{item?.description.slice(0, 240)}</p>
-                                <div className="card-actions justify-between">
-                                    <div className="btn btn-outline btn-success btn-xs"> <GiVote></GiVote>up vote</div>
-                                    <div className="btn btn-outline btn-success btn-xs"><GiVote></GiVote>down vote</div>
+                                </div>
+                                <div className="flex justify-between py-2 px-4">
+                                    <button className="btn btn-outline btn-success btn-xs" onClick={() => handlePollUp(item)}>
+                                    <GiVote></GiVote> up vote <p>{item.upVote} </p>
+                                    </button>
+                                    
+
                                 </div>
                             </div>
                         </div>
